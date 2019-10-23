67ef61e16f417ee2c8f34713379fa7ff295d94e1
Gio Cruz <giogodo@gmail.com>
2019-10-23 00:27:48 -0600

    CreateQuestion layout completed

--- src/components/CreateQuestion.js
+++ src/components/CreateQuestion.js
@@ -6,13 +6,16 @@ import Input from 'muicss/lib/react/input';
 import Button from 'muicss/lib/react/button';
 
 const mainPanelStyle = {
+  display: 'table',
+  tableLayout: 'fixed',
+  width: "100%",
   margin: 10,
-  width: 620,
-  display: 'flex',
+  maxWidth: 620,
   flexDirection: 'column'
 };
 const headerStyle = {
-  marginBottom: 10
+  marginBottom: 10,
+  padding: 10
 };
 const contentStyle = {
   marginTop: 10,
@@ -20,36 +23,54 @@ const contentStyle = {
   display: 'flex',
   flexDirection: 'column'
 };
+const labelStyle = {
+  marginTop: 10,
+  marginBottom: 15
+};
+const inputStyle = {
+  marginTop: 10,
+  marginBottom: 15
+};
+const orStyle = {
+  flex: 1,
+  display: 'flex',
+  justifyContent: 'center',
+  marginTop: 25
+};
+const buttonStyle = {
+  marginTop: 25
+};
 
 class CreateQuestion extends Component {
+  state = {
+    optionA: '',
+    optionB: ''
+  }
+
   render() {
     return (
       <Panel style={mainPanelStyle}>
         <div style={headerStyle}>
-          <div className="mui--text-headline">
-            Create New Question
+          <div className="mui--text-display1">
+            <b>Create New Question</b>
           </div>
         </div>
         <Divider />
         <div style={contentStyle}>
-          <div className="mui--text-subhead">
+          <div className="mui--text-subhead" style={labelStyle}>
             Complete the question:
           </div>
-          <div className="mui--text-headline">
-            Would you rather ...
+          <div className="mui--text-headline" style={labelStyle}>
+            <b>Would you rather ...</b>
           </div>
-          <Input label="Input 1" floatingLabel={true} style={{
-            marginTop: 15,
-            marginBottom: 25
-          }} />
-          <div className="mui--text-subhead">
-            OR
+          <Input label="Enter Option One Text Here" floatingLabel={true} style={inputStyle} />
+          <div style={orStyle}>
+            <div className="mui--text-headline" >
+              <b>OR</b>
+            </div>
           </div>
-          <Input label="Input 1" floatingLabel={true} style={{
-            marginTop: 15,
-            marginBottom: 25
-          }} />
-          <Button color="primary">Submit</Button>
+          <Input label="Enter Option Two Text Here" floatingLabel={true} style={inputStyle} />
+          <Button color="primary" style={buttonStyle}>Submit</Button>
         </div>
       </Panel>
     );
